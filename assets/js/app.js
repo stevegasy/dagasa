c5757c6e632b89529ea459b01fa447c5d2314862
stevegasy <stevensrabehaja@icloud.com>
2020-05-28 12:18:57 +0200

    second commit * replace zendframework/zend-code with lamina * install webpack-encore, react, react-router, axios, babel * add favicon

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,9 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+// ./src/js/app.js
 
-// any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+ import React from 'react';
+ import ReactDOM from 'react-dom';
+ import { BrowserRouter as Router } from 'react-router-dom';
+ import '../css/app.css';
+ import Home from './components/Home';
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+ ReactDOM.render(<Router><Home /></Router>, document.getElementById('root'));
